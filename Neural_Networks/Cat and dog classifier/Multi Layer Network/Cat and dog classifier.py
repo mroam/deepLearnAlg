b2780de2ffaaa4ace5d56cdb479a335ebe03771c
gtier <griffin@Griffins-MBP.fios-router.home>
2018-04-25 22:43:51 -0400

    Updated Cat and dog classifier

--- Neural_Networks/Cat and dog classifier/Multi Layer Network/Cat and dog classifier.py
+++ Neural_Networks/Cat and dog classifier/Multi Layer Network/Cat and dog classifier.py
@@ -3,6 +3,10 @@
 """
 
 @author: gtier
+
+To use this NN call the NN_Model function.
+Also, to use this NN put the /train and /test1 folder in the same directory as the Cat and dog classifier.
+Note: The dev set should not be the same as the test set.
 """
 
 import numpy as np
@@ -21,10 +25,12 @@ def tanh(x):
     t = (np.exp(x) - np.exp(-x))/(np.exp(x) + np.exp(-x))
     return t
 
+#Relu is faster than tanh, but I havn't implemented it yet. TODO
 def relu(x):
     r = np.maximum(0, x)
     return r
 
+#Called by set_up_data()
 def loadTrainData(m):
     yVect = []
     imgMat = []
@@ -45,6 +51,7 @@ def loadTrainData(m):
     dataSet = (np.array(imgMat)/255, np.array(yVect).reshape(m))
     return dataSet
 
+#This is called by NN_Model
 def set_up_data(m):
     dataSet = loadTrainData(m)
     #dataSet = sklearn.datasets.make_moons(n_samples=m, noise=0.1)
@@ -55,10 +62,6 @@ def set_up_data(m):
     #dataSet = (tempX, tempY)
     return dataSet
 
-
-def plot_data(dataSet):
-    plt.scatter(dataSet[0][:, 0], dataSet[0][:, 1], c=dataSet[1], cmap=plt.cm.RdBu)
-  
 #n_h can be changed to alter number of hidden layers (change when calling NN_model)
 def init_params(X, n_h):
     W1 = np.random.randn(n_h, np.size(X, axis = 0)) * 0.01
@@ -66,8 +69,13 @@ def init_params(X, n_h):
     W2 = np.random.randn(1, n_h) * 0.01
     b2 = np.zeros((1,1))
     return W1, b1, W2, b2
-    
+
 #A2 is output layer
+#Y is a vector containing the correct answers
+#m is the number of columns in the training set matrix
+#lambd is the regularization parameter
+#Regularization penalizes large weights
+#Add regularization TODO
 def cost(A2, Y, m):
     c = (-1/m) * np.sum(np.log(A2) * Y + (1-Y)*np.log(1-A2))
     return c
@@ -81,7 +89,7 @@ def forward_prop(X, W1, b1, W2, b2):
     A2 = sigmoid(Z2)
     return A1, Z1, A2, Z2
 
-
+#This is called by NN_Model
 def back_prop(A1, A2, Z1, X, W2, Y, m):
     dZ2 = A2 - Y
     dW2 = (1/m)*dZ2.dot(A1.T)
@@ -91,33 +99,17 @@ def back_prop(A1, A2, Z1, X, W2, Y, m):
     db1 = (1/m)*np.sum(dZ1, axis=1, keepdims = True)
     return dW1, db1, dW2, db2
 
-
+#This is called by NN_Model
 def predict(X, W1, b1, W2, b2):
     A1, Z1, A2, Z2 = forward_prop(X, W1, b1, W2, b2)
     predictions = np.rint(A2)
     return predictions
 
-
-def plot_boundary(X, W1, b1, W2, b2, dataSet):
-    step = 0.025
-    x_min = np.amin(X[0, :]) - 1
-    x_max = np.amax(X[0, :]) + 1
-    y_min = np.amin(X[1, :]) - 1
-    y_max = np.amax(X[1, :]) + 1
-    xMesh, yMesh = np.meshgrid(np.arange(x_min, x_max, step), np.arange(y_min, y_max, step))
-    
-    #print(np.c_[xMesh.ravel(), yMesh.ravel()].T.shape)
-    predictions = predict(np.c_[xMesh.ravel(), yMesh.ravel()].T, W1, b1, W2, b2)
-    predictions = predictions.reshape(xMesh.shape)
-    #print(predictions.shape)
-    plt.contourf(xMesh, yMesh, predictions, cmap=plt.cm.RdBu, alpha=0.7)
-    plot_data(dataSet)
-    plt.show()
-
-def loadTestData(m):
+#Called by NN_Model
+def loadTestData(m, devM):
     yVect = []
     imgMat = []
-    for i in range((10000-m)):
+    for i in range(devM):
         if ((i % 2) == 0):
             toChoose = "cat"
             yVect.append(1)
@@ -125,21 +117,23 @@ def loadTestData(m):
             toChoose = "dog"
             yVect.append(0)
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        img = Image.open(dir_path + "/train/{0}.{1}.jpg".format(toChoose, (10000-i)))
+        img = Image.open(dir_path + "/train/{0}.{1}.jpg".format(toChoose, (12499-i)))
         img = img.resize((24, 24))
         imgVector = img.getdata()
         imgVector = np.array(imgVector)
         imgVector = imgVector.reshape((24*24*3, 1))
         imgMat.append(imgVector[:, 0])
-    dataSet = (np.array(imgMat)/255, np.array(yVect).reshape((10000-m)))
+    dataSet = (np.array(imgMat)/255, np.array(yVect).reshape(devM))
     return dataSet
 
-def test_accuracy(testDataX, testDataY, W1, b1, W2, b2, m):
+#Called by NN_Model
+#Takes a trained NN and tests its accuracy on the dev set.
+def test_accuracy(testDataX, testDataY, W1, b1, W2, b2, m, devM):
     predictions = predict(testDataX, W1, b1, W2, b2)
     right = 0
     total = 0
     #print(predictions[0, 1])
-    for i in range((10000-m)):
+    for i in range(devM):
         if (predictions[0, i] == testDataY[i]):
             right += 1
             total += 1
@@ -147,11 +141,13 @@ def test_accuracy(testDataX, testDataY, W1, b1, W2, b2, m):
             total +=1
     accuracy = (right/total) * 100
     return accuracy
-    
-    
 
 
-def NN_Model(iterations, learning_rate, n_h, m):
+
+#Main model function
+#Yet to make this NN_Model multi-hidden layer TODO
+#NN_Model(iterations, learning_rate, number of hidden units in hidden layer, number of items in training set, number of items in dev set)
+def NN_Model(iterations, learning_rate, n_h, m, devM):
     dataSet = set_up_data(m)
     X = dataSet[0].T
     Y = dataSet[1]
@@ -163,13 +159,13 @@ def NN_Model(iterations, learning_rate, n_h, m):
         c = cost(A2, Y, m)
         if((i % 100) == 0):
             print("Cost after iteration " + str(i) + ": " + str(c))
-        
+
         dW1, db1, dW2, db2 = back_prop(A1, A2, Z1, X, W2, Y, m)
         W1 = W1 - learning_rate * dW1
         b1 = b1 - learning_rate * db1
         W2 = W2 - learning_rate * dW2
         b2 = b2 - learning_rate * db2
-        
+
     #print(W1, b1, W2, b2)
     predictions = predict(X, W1, b1, W2, b2)
     right = 0
@@ -181,16 +177,15 @@ def NN_Model(iterations, learning_rate, n_h, m):
             total += 1
         else:
             total +=1
-    
+
     accuracy = (right/total) * 100
     #plot_boundary(X, W1, b1, W2, b2, dataSet)
     print("Train accuracy: " + str(accuracy) + "%")
-    testData = loadTestData(m)
+    testData = loadTestData(m, devM)
     testDataX = testData[0].T
     testDataY = testData[1]
-    testAccuracy = test_accuracy(testDataX, testDataY, W1, b1, W2, b2, m)
+    testAccuracy = test_accuracy(testDataX, testDataY, W1, b1, W2, b2, m, devM)
     print("Test accuracy: " + str(testAccuracy) + "%")
-#NN_Model(iterations, learning_rate, number of hidden layers, number of items in dataset)    
-NN_Model(5000, 0.04, 25, 9000)
-#loadTrainData(50)
 
+#NN_Model(iterations, learning_rate, number of hidden units in hidden layer, number of items in training set, number of items in dev set)
+NN_Model(20000, 0.04, 25, 10000, 2000)
